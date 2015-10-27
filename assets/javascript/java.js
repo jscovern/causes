777df9bb1c69a45b7a5d800d361a6b4e8edd7e04
Jamie Scovern <jamie.scovern@gmail.com>
2015-10-26 22:32:43 -0400

    made a few updates to index and some other spots.  pu a youtube video in index but it's causing errors and breaking js so need to ask jason tomorrow

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,8 +1,11 @@
-$("#toggleContentOnClick").click(function() {
-	$("ul").slideToggle();
-	// console.log("made it into the div");
-});
 
+$(document).ready(function(){
+	$("#toggleContentOnClick").click(function() {
+		$("ul").slideToggle();
+		console.log("got in here");
+	// console.log("made it into the div");
+	});
+});
 
 // $('#toggle-content-onclick').click(function() {
   // $('#some_paragraph').slideToggle();
